1e4aac548d81f5d13fd82824bfd7120f6ba8b3ae
EdDavid398 <edwarcamelo1138519@correo.itm.edu.co>
2026-03-25 13:57:00 -0500

    Modify pause message and add confirmation function

--- utils.cpp
+++ utils.cpp
@@ -12,8 +12,15 @@ void limpiarPantalla() {
 }
 
 void pausarYLimpiar() {
-    cout << "\nPresiona ENTER para ocultar la pantalla y continuar...";
+    cout << "\nPresiona ENTER para continuar...";
     cin.ignore();
     cin.get();
     limpiarPantalla();
+}
+
+bool confirmarAccion(const char* mensaje) {
+    char respuesta;
+    cout << mensaje << " (s/n): ";
+    cin >> respuesta;
+    return (respuesta == 's' || respuesta == 'S');
 }
